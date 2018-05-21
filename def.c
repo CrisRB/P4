1b6d3fa8e4578404251f96a0d489ddc8dc69906d
CrisRB <36510828+CrisRB@users.noreply.github.com>
2018-05-21 16:46:48 +0200

    generalitzat a tots els graus

--- def.c
+++ def.c
@@ -4,8 +4,8 @@
 #define tol 5e-7
 
 double p1(double);
-double pol(double, int, double, double);
-double dpol(double, int, double, double);
+double pol(double x, int n);
+double dpol(double, int);
 double mnewton(double, double, double);
 
 int main()
@@ -16,11 +16,9 @@ int main()
     int z;
     printf("Introdueix nombre d'iteracions màximes: ");
     scanf("%i", &z);
-    
-    double p0=1;
-    
+
     int particions=0;
-    double dist=0, puntsmig[particions];
+    double dist=0, puntsmig[n];
     int canvisdesigne=0;
     
     while(canvisdesigne<n)
@@ -36,7 +34,7 @@ int main()
 //            printf("nodes[%i]=%lf\n",l,nodes[l]);
         }
         
-        for(int h=0;h<particions;h++)
+        for(int h=0;h<n;h++)
         {
             puntsmig[h]=0;            
         }
@@ -48,20 +46,24 @@ int main()
             k+=dist;
         }
         
+        int t=0;
         for(int j=0;j<particions;j++)
         {
             double signe=0;
-            signe=pol(nodes[j],n-1,p0,nodes[j])*pol(nodes[j+1],n-1,p0,nodes[j+1]);
+            signe=pol(nodes[j],n-1)*pol(nodes[j+1],n-1);
             if(signe<0)
             {
                 printf("Hi ha una arrel entre %lf i %lf\n",nodes[j],nodes[j+1]);
-                puntsmig[j]=(nodes[j]+nodes[j+1])/2;
+                puntsmig[t]=(nodes[j]+nodes[j+1])/2;
+                printf("puntmig[%i] = %lf\n",t,puntsmig[t]);
                 canvisdesigne+=1;
+                t+=1;
             }
         }
+        printf("canvis de signe=%i\n",canvisdesigne);
     }
     
-    for(int j=0;j<particions;j++)
+    for(int j=0;j<n;j++)
     {
         double x1=puntsmig[j], x2=0;
 //         printf("puntsmig[%i]=%lf\n",j,puntsmig[j]);
@@ -69,7 +71,7 @@ int main()
         {
             x2=x1;
             printf("x1=%lf\n",x1);
-            x1=mnewton(x1,pol(x1,n-1,p0,x1),dpol(x1,n,p0,x1));
+            x1=mnewton(x1,pol(x1,n-1),dpol(x1,n));
             if(fabs(x2-x1)<=tol) 
             {
                 printf("Arrel està a %.16G\n", x1);
@@ -84,14 +86,28 @@ int main()
     return 0;
 }
 
-double pol(double x, int n, double a, double b) //b pol n-1, a pol n-2, aquí n és n-1
+double pol(double x, int n) //n és n-1
 {
-    return ((2*n+1)*x*b-(n*a))/(n+1);
+    double a=1,b=x,c=0;
+    for(int i=0;i<n;i++)
+    {
+        c=((2*n+1)*x*b-(n*a))/(n+1);
+        a=b;
+        b=c;
+    }
+    return c;
 }
 
-double dpol(double x, int n, double a, double b)    //aquí la n és la que toca
+double dpol(double x, int n)    //aquí la n és la que toca
 {
-    return (-n*x*b+n*a)/(1-x*x);
+    double a=1, b=x, c=0;
+    for(int i=0;i<n-1;i++)
+    {
+        c=(-n*x*b+n*a)/(1-x*x);
+        a=b;
+        b=c;
+    }
+    return c;
 }
 
 double mnewton(double xn, double pol, double dpol)
